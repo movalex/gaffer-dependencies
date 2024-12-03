9046a2aec34e57cb415f2c14ded8baa2e8f7966a
Alexey Bogomolov <mail@abogomolov.com>
2024-12-03 03:54:13 +0300

    use prebuilt macos binaries

--- Embree/config.py
+++ Embree/config.py
@@ -2,38 +2,17 @@
 
 	"downloads" : [
 
-		"https://github.com/embree/embree/archive/v4.3.2.tar.gz"
-
+		"https://github.com/RenderKit/embree/releases/download/v4.3.3/embree-4.3.3.x86_64.macosx.zip"
 	],
 
 	"url" : "https://www.embree.org/",
 
-	"license" : "LICENSE.txt",
-
 	"dependencies" : [ "TBB" ],
 
 	"commands" : [
-
-		"mkdir gafferBuild",
-		"cd gafferBuild &&"
-			" cmake"
-			" -D CMAKE_INSTALL_PREFIX={buildDir}"
-			" -D CMAKE_PREFIX_PATH={buildDir}"
-			" -D CMAKE_INSTALL_LIBDIR={buildDir}/lib"
-			" -D EMBREE_TBB_ROOT={buildDir}"
-			" -D EMBREE_STATIC_LIB=OFF"
-			" -D EMBREE_ISPC_SUPPORT=OFF"
-			" -D EMBREE_TUTORIALS=OFF"
-			" -D EMBREE_RAY_MASK=ON"
-			" -D EMBREE_FILTER_FUNCTION=ON"
-			" -D EMBREE_BACKFACE_CULLING=OFF"
-			" -D EMBREE_BACKFACE_CULLING_CURVES=ON"
-			" -D EMBREE_BACKFACE_CULLING_SPHERES=ON"
-			" -D EMBREE_NO_SPLASH=ON"
-			" -D EMBREE_TASKING_SYSTEM=TBB"
-			" ..",
-		"cd gafferBuild && make install -j {jobs} VERBOSE=1",
-
+		"rsync -av ./* {buildDir}",
+        "wget https://raw.githubusercontent.com/RenderKit/embree/refs/heads/master/LICENSE.txt",
+        "cp LICENSE.txt {buildDir}/doc/licenses/Embree"
 	],
 
 	"manifest" : [
--- LLVM/config.py
+++ LLVM/config.py
@@ -2,35 +2,16 @@
 
 	"downloads" : [
 
-		"https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/llvm-15.0.7.src.tar.xz",
-		"https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/clang-15.0.7.src.tar.xz",
-		"https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/cmake-15.0.7.src.tar.xz"
+		"https://github.com/llvm/llvm-project/releases/download/llvmorg-19.1.3/LLVM-19.1.3-macOS-X64.tar.xz",
 
 	],
 
 	"url" : "https://llvm.org",
 
-	"license" : "LICENSE.TXT",
-
 	"commands" : [
-
-		"mv ../clang* tools/clang",
-		"mv ../cmake* ../cmake",
-		"mkdir build",
-		"cd build &&"
-			" cmake"
-			" -DCMAKE_INSTALL_PREFIX={buildDir}"
-			" -DGCC_INSTALL_PREFIX={compilerRoot}"
-			" -DCMAKE_BUILD_TYPE=Release"
-			" -DLLVM_ENABLE_RTTI=ON"
-			" -DLLVM_ENABLE_LIBXML2=OFF"
-			" -DLLVM_ENABLE_ZSTD=OFF"
-			" -DLLVM_INCLUDE_BENCHMARKS=OFF"
-			" -DLLVM_INCLUDE_TESTS=OFF"
-			" -DLLVM_TARGETS_TO_BUILD={buildTargets}"
-			" ..",
-		"cd build && make install -j {jobs}"
-
+		"rsync -av ./* {buildDir}",
+		"wget https://raw.githubusercontent.com/llvm/llvm-project/refs/heads/main/LICENSE.TXT",
+        "cp LICENSE.TXT {buildDir}/doc/licenses/LLVM"
 	],
 
 	"variables" : {
